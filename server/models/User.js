a9146bb93c6b0e15749bec3e82090c8b4b7f85fb
zishanx <zishanalikhan53@gmail.com>
2026-08-26 18:00:00 +0530

    Update user schema

--- server/models/User.js
+++ server/models/User.js
@@ -2,8 +2,9 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String, required: true , select: false},
     subscription: { type: String, enum: ["Free", "Premium"], default: "Free" },
 })
 
