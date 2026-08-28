c7b9dcbad575a4d9c6be472be22a1faa3a0a45c2
zishanx <zishanalikhan53@gmail.com>
2026-08-28 18:00:00 +0530

    Add user registration function

---
+++ server/controllers/authController.js
@@ -0,0 +1,23 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
+
+const register = async (req, res) => {
+    const { name, username, email, password } = req.body;
+    const UserExists = await User.findOne({
+        email, username
+    });
+
+    if (UserExists) {
+        return res.status(400).json({ message: 'User already exists' });
+    } else {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({
+            name,
+            username,
+            email,
+            password: hashedPassword
+        });
+        res.status(201).json({ message: 'User created successfully', user });
+    }
+}
