34e8bf6e5fa74e07f6870965fa060b52d7da526d
zishanx <zishanalikhan53@gmail.com>
2026-08-31 18:00:00 +0530

    Add protect middleware

--- server/middleware/protect.js
+++ server/middleware/protect.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken'
+import User from '../models/User.js'
 
 export const protect = async (req, res, next) => {
     const token = req.headers.authorization?.split(" ")[1];
@@ -6,10 +7,13 @@ export const protect = async (req, res, next) => {
     try {
         if (token) {
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            req.user = decoded
+            req.user = await User.findById(decoded.user_id)
+            if (!req.user) {
+                return res.status(401).json({ message: "no user found" })
+            }
             next()
         } else {
-            res.status(400).json({ message: "token not found" });
+            res.status(401).json({ message: "token not found" });
         }
 
 
