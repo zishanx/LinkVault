37db2de469c6eebc228b6f7c083c9a26af015c69
zishanx <zishanalikhan53@gmail.com>
2026-09-03 23:20:14 +0530

    Done with the Link model

--- server/models/Link.js
+++ server/models/Link.js
@@ -3,7 +3,11 @@ import mongoose from 'mongoose';
 const linkSchema = new mongoose.Schema({
     name: { type: String, required: true },
     link: { type: String, required: true },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', unique: true }
-})
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    clickCount: { type: Number, default: 0 },
+    order: { type: Number, required: true }
+}, { timestamps: true })
 
-const Link  = mongoose.model('Link', linkSchema)
+const Link = mongoose.model('Link', linkSchema)
+
+export default Link
