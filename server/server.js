30663a4fe137bf08caffe1ea2eeccd45876a9b9d
zishanx <zishanalikhan53@gmail.com>
2026-09-04 22:03:03 +0530

    rolled up the createLink to the server and linkRoutes

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ import express from 'express'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 import cors from 'cors'
-import authRoutes from './routes/auth.js'
-
+import authRoutes from './routes/authRoutes.js'
+import linkRoutes from './routes/linkRoutes.js'
 
 dotenv.config()
 
@@ -12,6 +12,7 @@ app.use(express.json())
 app.use(cors())
 
 app.use('/api/auth', authRoutes)
+app.use('/api/links',linkRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
