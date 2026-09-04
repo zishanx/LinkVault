35b4bb8d7e1f818c81a98f189052b009587fd73d
zishanx <zishanalikhan53@gmail.com>
2026-09-04 21:20:36 +0530

    Working on createLink

---
+++ server/controllers/linkController.js
@@ -0,0 +1,21 @@
+import Link from '../models/Link.js';
+
+
+//createLink 
+
+const createLink = async (req, res) => {
+    const { name, link } = req.body;
+    const user = req.user.user._id;
+
+    try {
+        const order = await Link.countDocuments({ user })
+
+        const createdLink = await Link.create({ name, link, user, order })
+
+        res.status(200).json({link:createdLink, message: "Link created successfully ."})
+
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+
+}
