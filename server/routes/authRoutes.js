d25f54818f330ae4da5d1b6ba2a757cac7f4acd7
zishanx <zishanalikhan53@gmail.com>
2026-09-09 00:06:26 +0530

    done with the auth Veify

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,11 +1,14 @@
 import express from 'express'
-import { register, login } from "../controllers/authController.js"
+import { register, login, verifyAuth } from "../controllers/authController.js"
+import { protect } from '../middleware/protect.js'
 
 const router = express.Router()
 
 router.post('/register', register)
 
 router.post('/login', login)
+
+router.get('/verify',protect, verifyAuth)
 
 
 export default router
