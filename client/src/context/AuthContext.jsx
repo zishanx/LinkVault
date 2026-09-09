ab0905e5c82e7cbff6a210a71eaa4cdef8e1e2eb
zishanx <zishanalikhan53@gmail.com>
2026-09-09 23:52:56 +0530

    Working on the context

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,50 @@
+import { useEffect } from "react";
+import { createContext, useContext, useState } from "react";
+
+// creating the context ("the box");
+
+const AuthContext = createContext()
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [token, setToken] = useState(localStorage.getItem('token') || null)
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        const verify = async () => {
+            const res = await fetch('', {
+                method: "GET",
+                headers: { "Authorization": token, "Content-type": "application/json" },
+            })
+        }
+
+        verify()
+    }, [])
+
+
+    const login = (newToken, userData) => {
+        localStorage.setItem('token', newToken);
+        setToken(newToken);
+        localStorage.setItem('user', JSON.stringify(userData))
+        setUser(userData)
+    }
+
+
+    const logout = () => {
+        localStorage.removeItem('token')
+        localStorage.removeItem('user')
+        setUser(null)
+        setToken(null)
+    }
+
+    const value = { user, token, isLoading, login, logout };
+
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+
+export const useAuth = () => useContext(AuthContext)
