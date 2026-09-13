6466ba97d04227aff320554d997e1a44b63d8b8a
zishanx <zishanalikhan53@gmail.com>
2026-09-13 21:27:59 +0530

    the login form

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,84 +1,91 @@
-import { useState } from "react"
-import api from '../api/axios.js'
-import { useAuth } from "../context/AuthContext"
+// import everything that we are going to need. 
+import { useState } from "react";
+import { useAuth } from '../context/AuthContext'
 import { useNavigate } from 'react-router-dom'
+import api from '../api/axios.js'
+
+
+
 
 export default function Login() {
 
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [error, setError] = useState(null)
+    const [form, setForm] = useState({ email: "", password: "" })
     const [isLoading, setIsLoading] = useState(false)
-
+    const [error, setError] = useState(null)
     const { login } = useAuth()
     const navigate = useNavigate()
 
 
-    const handleSubmit = async (e) => {
 
+    const handleChange = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = async (e) => {
         e.preventDefault()
-        setError(null)
         setIsLoading(true)
+        setError(null)
         try {
-            const res = await api.post('/auth/login', { email, password })
+            const res = await api.post('/auth/login', form)
+
             login(res.data.token, res.data.user)
-            navigate('/home')
-        } catch (err) {
-            setError(err.response?.data?.message || "Something went wrong")
+            navigate('/')
+        } catch (error) {
+            setError(error.response?.data?.message || "Something went wrong")
+
         } finally {
             setIsLoading(false)
         }
-
     }
+
+
 
     return (
         <>
-            <h1>Login</h1>
+            <div>
 
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input
-                        id="email"
-                        type="text"
-                        name="email"
-                        value={email}
-                        onChange={(e) => {
-                            setEmail(e.target.value)
-                        }}
-                        required
-                        placeholder="johndoe@gmail.com"
-                    />
-                </div>
 
 
                 <div>
-                    <label htmlFor="password">Password</label>
-                    <input
-                        id="password"
-                        name="password"
-                        type="password"
-                        value={password}
-                        onChange={(e) => {
-                            setPassword(e.target.value)
-                        }}
-                        required
-                        placeholder="*******"
-                    />
+
+                    <h1>Login</h1>
+                    <form
+                        onSubmit={handleSubmit}
+                    >
+                        <div>
+                            <label htmlFor="email">Email</label>
+                            <input
+                                type="text"
+                                id="email"
+                                name="email"
+                                placeholder="johndoe@gmail.com"
+                                value={form.email}
+                                onChange={(e) => { handleChange(e) }}
+                                required
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="password">Password</label>
+                            <input
+                                type="password"
+                                id="password"
+                                name="password"
+                                placeholder="******"
+                                value={form.password}
+                                onChange={(e) => { handleChange(e) }}
+                                required
+                            />
+                        </div>
+
+                        <button type="submit" disabled={isLoading}>Login</button>
+
+                        {error ? (<div>{error}</div>) : <></>}
+                    </form>
+                </div>
+                <div>
 
                 </div>
-
-                <button
-                    type="submit"
-                    disabled={isLoading}
-                >Log in</button>
-
-                {error ? (<div><p>{error}</p></div>) : <></>}
-
-            </form>
-
+            </div>
         </>
     )
-
 }
-
