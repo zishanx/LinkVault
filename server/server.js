9ff0bbf4c9095804ed85362ce9cf6661e0dd765f
zishanx <zishanalikhan53@gmail.com>
2026-09-19 22:33:39 +0530

    Wokring with max mind

--- server/server.js
+++ server/server.js
@@ -4,18 +4,20 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import authRoutes from './routes/authRoutes.js'
 import linkRoutes from './routes/linkRoutes.js'
+import { loadGeoDB } from './utils/geoip.js'
+
 
 dotenv.config()
 
 const app = express()
 app.use(express.json())
 app.use(cors())
-
 app.use('/api/auth', authRoutes)
-app.use('/api/links',linkRoutes)
+app.use('/api/links', linkRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
+    .then(() => loadGeoDB()).then(() => {
+
         app.listen(process.env.PORT, () => {
             console.log("Server running on port 3000");
             console.log("MongoDB connected!");
---
+++ server/utils/geoip.js
@@ -0,0 +1,12 @@
+import maxmind from 'maxmind';
+
+let lookup; 
+
+export const loadGeoDB = async () => {
+    lookup = await maxmind.open('./data/GeoLite2-Country.mmd');
+}
+
+export const getCountry = (ip) => {
+    const result = lookup?.get(ip);
+    return result?.country?.iso_code;
+}
