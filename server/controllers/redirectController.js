30c20fb183919cf9d3ed987506c39f77d7781005
zishanx <zishanalikhan53@gmail.com>
2026-09-22 20:23:22 +0530

    done with the redirect| git push

--- server/controllers/redirectController.js
+++ server/controllers/redirectController.js
@@ -4,16 +4,24 @@ import { getCountry } from "../utils/geoip";
 
 export const redirect = async (req, res) => {
 
-    const id = req.param.id
+    const id = req.params.id
 
-    const link = await Link.findById(id)
-
-    if (link) {
-        res.redirect(link.link)
-
-        const country = getCountry(req.ip)
-        link.clickCount++
-        Click.create({ id, country })
+    try {
+        const link = await Link.findById(id)
+        if (link) {
+            res.redirect(link.link)
+            const country = getCountry(req.ip)
+            link.clickCount++
+            link.save().catch(err => console.log(err))
+            Click.create({ link: id, country }).catch(err => console.log(err))
+        } else {
+            res.status(404).json({ message: "Invalid Link!" })
+        }
+    } catch (error) {
+        console.log(error)
     }
+
+
+
 
 }
