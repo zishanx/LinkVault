9b38979c7783ae298edb3d45ab2b74d261345bb8
zishanx <zishanalikhan53@gmail.com>
2026-09-22 20:13:19 +0530

    Working with the link

--- server/controllers/redirectController.js
+++ server/controllers/redirectController.js
@@ -1,5 +1,19 @@
+import Click from "../models/Click";
+import Link from '../models/Link'
+import { getCountry } from "../utils/geoip";
 
+export const redirect = async (req, res) => {
 
-export default function redirect  (){
+    const id = req.param.id
+
+    const link = await Link.findById(id)
+
+    if (link) {
+        res.redirect(link.link)
+
+        const country = getCountry(req.ip)
+        link.clickCount++
+        Click.create({ id, country })
+    }
 
 }
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import authRoutes from './routes/authRoutes.js'
 import linkRoutes from './routes/linkRoutes.js'
+import redirectRoutes from './routes/redirectroute.js'
 import { loadGeoDB } from './utils/geoip.js'
 
 
@@ -14,6 +15,7 @@ app.use(express.json())
 app.use(cors())
 app.use('/api/auth', authRoutes)
 app.use('/api/links', linkRoutes)
+app.use('/r', redirectRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => loadGeoDB()).then(() => {
